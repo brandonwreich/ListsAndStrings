dc0ebfcae2c3e9a033c791822e3c53ed1a7640b4
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2017-10-05 09:21:12 -0600

    Added getters and setters

--- src/useful/model/Donut.java
+++ src/useful/model/Donut.java
@@ -14,21 +14,36 @@ public class Donut
 	{
 		this.flavor = "plain";
 		this.shape = "round";
+		this.holeCount = 1;
 		this.hasSprinkles = false;
 		this.isGlazed = false;
 	}
 	
-	public Donut(String flavor)
+	public Donut(String flavor, String shape)
 	{
 		this();
 		this.flavor = flavor;
-//		this.shape = shape;
+		this.shape = shape;
 //		this.hasSprinkles = hasSprinkles;
 //		this.isGlazed = isGlazed;
 //		this.holeCount = holeCount;
 	}
 	
-	public String toString()
+	public String getShape()
+	{
+		return shape;
+	}
+	public String getFlavor()
+	{
+		return flavor;
+	}
+	
+	public void setShape(String shape)
+	{
+		this.shape = shape;
+	}
+	
+public String toString()
 	{
 		String description = "This donut is a " + flavor + " donut";
 		
